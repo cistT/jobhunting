1b78a1a17959ec19277e0c854b4fd33f198b3f99
cistT <cist.b220.t.chitose@gmail.com>
2022-08-06 15:03:34 +0900

    メタデータを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import type { NextPage } from "next";
 import Header from "components/organisms/Header";
+import Head from "next/head";
 
-const Home: NextPage = () => <Header />;
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>就活管理アプリ</title>
+    </Head>
+    <Header />
+  </>
+);
 
 export default Home;
