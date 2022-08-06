2487cba5c61752c3bd23a06bd4420fa21d999095
cistT <cist.b220.t.chitose@gmail.com>
2022-08-06 17:35:42 +0900

    フォルダを削除

--- src/components/atoms/Button/LoginButton/__test__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import LoginButton from "..";
-
-it("LoginButton", () => {
-  const onClick = () => undefined;
-  render(<LoginButton onClick={onClick} />);
-  screen.debug();
-});
