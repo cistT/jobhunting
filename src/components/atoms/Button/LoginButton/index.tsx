597b50f5a0fcb07a3c29a5e2cbe3793c265e183c
cistT <cist.b220.t.chitose@gmail.com>
2022-08-07 17:58:16 +0900

    IconButtonを呼び出して、LoginButtonを作成するように変更

--- src/components/atoms/Button/LoginButton/index.tsx
+++ src/components/atoms/Button/LoginButton/index.tsx
@@ -1,29 +1,12 @@
 import React from "react";
 
-import { css } from "@emotion/react";
-
-import { Button } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 
-import ButtonProps from "../type/index";
+import IconButton from "../IconButton";
 
-const LoginButton = (props: ButtonProps) => {
-  const { onClick, className = "" } = props;
+import { ButtonProps } from "../type/index";
 
-  return (
-    <Button css={[styles.button]} className={className} onClick={onClick}>
-      <div>
-        <LoginIcon />
-        <div>ログイン</div>
-      </div>
-    </Button>
-  );
-};
-
+const LoginButton = (props: ButtonProps) => (
+  <IconButton {...props} icon={<LoginIcon />} label="ログイン" />
+);
 export default LoginButton;
-
-const styles = {
-  button: css`
-    text-align: center;
-  `,
-};
