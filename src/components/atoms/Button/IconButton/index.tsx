a145e3cc35aa4862ce0570b22471e593c4537d01
cistT <cist.b220.t.chitose@gmail.com>
2022-08-07 17:50:09 +0900

    Propsのtextをlabelに変更

--- src/components/atoms/Button/IconButton/index.tsx
+++ src/components/atoms/Button/IconButton/index.tsx
@@ -7,13 +7,13 @@ import { Button } from "@mui/material";
 import { IconButtonProps } from "../type/index";
 
 const IconButton = (props: IconButtonProps) => {
-  const { onClick, icon, text = "", className = "" } = props;
+  const { onClick, icon, label = "", className = "" } = props;
 
   return (
     <Button css={[styles.button]} className={className} onClick={onClick}>
       <div>
         {icon}
-        <div>{text}</div>
+        <div>{label}</div>
       </div>
     </Button>
   );
