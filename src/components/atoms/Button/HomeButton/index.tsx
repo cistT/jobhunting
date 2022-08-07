ce9ba3c94b77aa1fbc7c8bae8c763120572da01b
cistT <cist.b220.t.chitose@gmail.com>
2022-08-07 17:35:24 +0900

    Home画面に遷移するボタンを作成

---
+++ src/components/atoms/Button/HomeButton/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import HomeIcon from "@mui/icons-material/Home";
+
+import IconButton from "../IconButton";
+
+import ButtonProps from "../type/index";
+
+const HomeButton = (props: ButtonProps) => {
+  return <IconButton {...props} icon={<HomeIcon />} />;
+};
+
+export default HomeButton;
