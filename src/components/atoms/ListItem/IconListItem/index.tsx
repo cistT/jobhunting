9327175c73869e0e3c32f6140c9f9a80fa581db4
cistT <cist.b220.t.chitose@gmail.com>
2022-08-10 20:48:05 +0900

    Iconと文字を表示するListItemを作成

---
+++ src/components/atoms/ListItem/IconListItem/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { css } from "@emotion/react";
+
+import { ListItem, ListItemText } from "@mui/material";
+
+import ListItemProps from "../type";
+
+const IconListItem = (props: ListItemProps) => {
+  const { icon, label, onClick = () => undefined, className = "" } = props;
+  return (
+    <>
+      <ListItem css={styles.listItem} className={className} onClick={onClick}>
+        {icon}
+        <ListItemText primary={label} />
+      </ListItem>
+    </>
+  );
+};
+
+export default IconListItem;
+
+const styles = {
+  listItem: css`
+    height: 60px;
+    width: 200px;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.5s;
+    color: #5f76f8;
+    :hover {
+      background-color: rgba(0, 0, 0, 0.05);
+    }
+  `,
+};
