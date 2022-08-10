106b79e0884f026e4bf8bc2e6408ae9251605a8f
cistT <cist.b220.t.chitose@gmail.com>
2022-08-10 20:47:03 +0900

    プロップスにiconを追加

--- src/components/molecules/List/type/index.d.ts
+++ src/components/molecules/List/type/index.d.ts
@@ -1,6 +1,9 @@
 import { React } from "react";
 type ListProps = {
   items: {
+    icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+      muiName: string;
+    };
     label: string;
     onClick: React.MouseEventHandler<HTMLButtonElement>;
   }[];
