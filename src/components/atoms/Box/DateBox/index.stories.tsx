207d0eca6a378e16f5e9e495a2fe9876a8dde854
cistT <cist.b220.t.chitose@gmail.com>
2022-08-11 09:24:38 +0900

    DateBoxのStorybookを作成

---
+++ src/components/atoms/Box/DateBox/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { ComponentMeta } from "@storybook/react";
+
+import DateBox from ".";
+
+export default {
+  title: "Box/DateBox",
+  component: DateBox,
+} as ComponentMeta<typeof DateBox>;
+
+export const Original = () => {
+  const date = new Date();
+  return <DateBox date={date} />;
+};
