2631fcc54e5f936c7ee5b3aadb1dcdbd418fa991
cistT <cist.b220.t.chitose@gmail.com>
2022-08-11 10:39:37 +0900

    ホーム画面で表示させるコンテンツを作成

---
+++ src/components/organisms/HomeContents/index.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+import { css } from "@emotion/react";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ListIcon from "@mui/icons-material/List";
+import AddIcon from "@mui/icons-material/Add";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { Divider, Typography } from "@mui/material";
+
+import List from "components/molecules/List";
+import CompanyList, { CompanyListProps } from "../CompanyList";
+
+import useWindowSize from "hooks/useWindowSize";
+
+export type HomeContentsProps = CompanyListProps;
+
+const HomeContents = (props: HomeContentsProps) => {
+  const { companies } = props;
+
+  const { width } = useWindowSize();
+  const onClickHoge = () => undefined;
+  const items = [
+    {
+      icon: <HomeIcon />,
+      label: "Home",
+      onClick: onClickHoge,
+    },
+    {
+      icon: <ListIcon />,
+      label: "企業一覧",
+      onClick: onClickHoge,
+    },
+    {
+      icon: <AddIcon />,
+      label: "企業の追加",
+      onClick: onClickHoge,
+    },
+    {
+      icon: <CalendarMonthIcon />,
+      label: "予定",
+      onClick: onClickHoge,
+    },
+  ];
+  return (
+    <div css={width > 600 && styles.container}>
+      {width > 600 && (
+        <div css={styles.leftContainer}>
+          <div></div>
+          <List items={items} />
+          <Divider />
+        </div>
+      )}
+      <div css={styles.rightContainer}>
+        <Typography variant="h1" component="h1" css={styles.title}>
+          直近の予定
+        </Typography>
+        <Divider />
+        <CompanyList companies={companies} />
+      </div>
+    </div>
+  );
+};
+
+export default HomeContents;
+
+const styles = {
+  container: css`
+    display: grid;
+    grid-template-columns: 25vw 1fr;
+
+    gap: 10px;
+  `,
+  leftContainer: css`
+    display: grid;
+    grid-template-rows: 40px 80px 80px 80px 80px 0px;
+    gap: 2.5px;
+  `,
+  rightContainer: css`
+    text-align: center;
+  `,
+  listItem: css`
+    width: 30vw;
+  `,
+  title: css`
+    font-size: 32px;
+    margin-bottom: 10px;
+  `,
+};
