abc56a035aadfad93c924b564964b5f0839d542c
cistT <cist.b220.t.chitose@gmail.com>
2022-08-15 20:41:03 +0900

    企業名と日付をリスト形式で表示するコンポーネントを作成

---
+++ src/components/organisms/CompanyDateList/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import DateBox from "components/atoms/Box/DateBox";
+import CompanyListItem from "components/molecules/CompanyListItem";
+
+import { companiesType } from "types";
+
+export type CompanyDateListProps = {
+  companies: (companiesType & {
+    onClick?: React.MouseEventHandler<HTMLLIElement>;
+    className?: string;
+  })[];
+  className?: string;
+};
+
+const CompanyList = (props: CompanyDateListProps) => {
+  const { companies, className = "" } = props;
+
+  return (
+    <div className={className}>
+      {companies.map(({ name, interviewDate, className, id }) => (
+        <CompanyListItem
+          label={name}
+          rightItem={<DateBox date={interviewDate} />}
+          className={className}
+          key={id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CompanyList;
