b1e7a5e9c3c14b71a52aa06aca3f025e5fe11337
cistT <cist.b220.t.chitose@gmail.com>
2022-08-15 20:32:37 +0900

    Itemの右側にコンポーネントを自由に設置できるように修正

--- src/components/molecules/CompanyListItem/index.tsx
+++ src/components/molecules/CompanyListItem/index.tsx
@@ -2,25 +2,31 @@ import React from "react";
 
 import { css } from "@emotion/react";
 
-import DateBox from "components/atoms/Box/DateBox";
 import { Divider, ListItem, Typography } from "@mui/material";
 
 export type CompanyListItemProps = {
   label: string;
-  date: Date;
+  rightItem?: React.ReactElement<any, any>;
   onClick?: React.MouseEventHandler<HTMLLIElement>;
   className?: string;
 };
 
 const CompanyListItem = (props: CompanyListItemProps) => {
-  const { label, date, onClick = () => undefined, className = "" } = props;
+  const {
+    label,
+    rightItem = undefined,
+    onClick = () => undefined,
+    className = "",
+  } = props;
   return (
     <>
       <ListItem css={styles.container} onClick={onClick} className={className}>
-        <Typography variant="h3" component="h3" css={styles.label}>
-          {label}
-        </Typography>
-        <DateBox date={date} />
+        <>
+          <Typography variant="h3" component="h3" css={styles.label}>
+            {label}
+          </Typography>
+          {rightItem}
+        </>
       </ListItem>
 
       <Divider />
