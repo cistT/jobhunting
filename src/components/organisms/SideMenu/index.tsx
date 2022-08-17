3cdf3974f683308a0c4fdc82247391fa885ccddb
cistT <cist.b220.t.chitose@gmail.com>
2022-08-17 17:00:29 +0900

    画面の左側に表示されるメニューを作成。

---
+++ src/components/organisms/SideMenu/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { css } from "@emotion/react";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ListIcon from "@mui/icons-material/List";
+import AddIcon from "@mui/icons-material/Add";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { Divider } from "@mui/material";
+
+import IconList from "components/molecules/IconList";
+
+const SideMenu = () => {
+  const onClickHoge = () => undefined;
+  const items = [
+    {
+      icon: <HomeIcon />,
+      label: "Home",
+      onClick: onClickHoge,
+    },
+    {
+      icon: <ListIcon />,
+      label: "企業一覧",
+      onClick: onClickHoge,
+    },
+    {
+      icon: <AddIcon />,
+      label: "企業の追加",
+      onClick: onClickHoge,
+    },
+    {
+      icon: <CalendarMonthIcon />,
+      label: "予定",
+      onClick: onClickHoge,
+    },
+  ];
+  return (
+    <div css={styles.container}>
+      <div></div>
+      <IconList items={items} />
+      <Divider />
+    </div>
+  );
+};
+
+export default SideMenu;
+
+const styles = {
+  container: css`
+    display: grid;
+    grid-template-rows: 40px 240px 0px;
+    gap: 2.5px;
+  `,
+};
