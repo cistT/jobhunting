6ecec6abf57fb9a9ae5705e61096409021687bd7
cistT <cist.b220.t.chitose@gmail.com>
2022-08-17 16:59:52 +0900

    ホーム画面に表示するコンテンツを作成。企業名と日付がリスト形式で表示される。

--- src/components/organisms/HomeContents/index.tsx
+++ src/components/organisms/HomeContents/index.tsx
@@ -2,62 +2,22 @@ import React from "react";
 
 import { css } from "@emotion/react";
 
-import HomeIcon from "@mui/icons-material/Home";
-import ListIcon from "@mui/icons-material/List";
-import AddIcon from "@mui/icons-material/Add";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import { Divider, Typography } from "@mui/material";
 
-import IconList from "components/molecules/IconList";
 import CompanyDateList, { CompanyDateListProps } from "../CompanyDateList";
-
-import useWindowSize from "hooks/useWindowSize";
 
 export type HomeContentsProps = CompanyDateListProps;
 
 const HomeContents = (props: HomeContentsProps) => {
   const { companies } = props;
 
-  const { width } = useWindowSize();
-  const onClickHoge = () => undefined;
-  const items = [
-    {
-      icon: <HomeIcon />,
-      label: "Home",
-      onClick: onClickHoge,
-    },
-    {
-      icon: <ListIcon />,
-      label: "企業一覧",
-      onClick: onClickHoge,
-    },
-    {
-      icon: <AddIcon />,
-      label: "企業の追加",
-      onClick: onClickHoge,
-    },
-    {
-      icon: <CalendarMonthIcon />,
-      label: "予定",
-      onClick: onClickHoge,
-    },
-  ];
   return (
-    <div css={width > 600 && styles.container}>
-      {width > 600 && (
-        <div css={styles.leftContainer}>
-          <div></div>
-          <IconList items={items} />
-          <Divider />
-        </div>
-      )}
-      <div css={styles.rightContainer}>
-        <Typography variant="h1" component="h1" css={styles.title}>
-          直近の予定
-        </Typography>
-        <Divider />
-        <CompanyDateList companies={companies} />
-      </div>
+    <div css={styles.container}>
+      <Typography variant="h1" component="h1" css={styles.title}>
+        直近の予定
+      </Typography>
+      <Divider />
+      <CompanyDateList companies={companies} />
     </div>
   );
 };
@@ -66,20 +26,7 @@ export default HomeContents;
 
 const styles = {
   container: css`
-    display: grid;
-    grid-template-columns: 25vw 1fr;
-    gap: 20px;
-  `,
-  leftContainer: css`
-    display: grid;
-    grid-template-rows: 40px 240px 0px;
-    gap: 2.5px;
-  `,
-  rightContainer: css`
     text-align: center;
-  `,
-  listItem: css`
-    width: 30vw;
   `,
   title: css`
     font-size: 32px;
