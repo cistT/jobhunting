3c19c5cdb2877b581fc3d9ef552eac96ce2ad6fa
cistT <cist.b220.t.chitose@gmail.com>
2022-08-17 19:31:28 +0900

    リストのタイトルをListTItleコンポーネントを使って表示

--- src/components/organisms/HomeContents/index.tsx
+++ src/components/organisms/HomeContents/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-import { css } from "@emotion/react";
-
-import { Divider, Typography } from "@mui/material";
-
+import ListTitle from "components/molecules/ListTitle";
 import CompanyDateList, { CompanyDateListProps } from "../CompanyDateList";
 
 export type HomeContentsProps = CompanyDateListProps;
@@ -12,24 +9,11 @@ const HomeContents = (props: HomeContentsProps) => {
   const { companies } = props;
 
   return (
-    <div css={styles.container}>
-      <Typography variant="h1" component="h1" css={styles.title}>
-        直近の予定
-      </Typography>
-      <Divider />
+    <>
+      <ListTitle title="直近の予定" />
       <CompanyDateList companies={companies} />
-    </div>
+    </>
   );
 };
 
 export default HomeContents;
-
-const styles = {
-  container: css`
-    text-align: center;
-  `,
-  title: css`
-    font-size: 32px;
-    margin-bottom: 10px;
-  `,
-};
