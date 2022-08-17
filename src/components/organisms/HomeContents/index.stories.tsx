77d017a18544f7281767a1018460b6172a3a1ebe
cistT <cist.b220.t.chitose@gmail.com>
2022-08-17 19:31:50 +0900

    企業情報をテストデータに変更

--- src/components/organisms/HomeContents/index.stories.tsx
+++ src/components/organisms/HomeContents/index.stories.tsx
@@ -3,21 +3,13 @@ import React from "react";
 import { ComponentMeta } from "@storybook/react";
 
 import HomeContents from ".";
+import testData from "data/testData";
 
 export default {
   title: "Organisms/HomeContents",
   component: HomeContents,
 } as ComponentMeta<typeof HomeContents>;
 
-const dummyData = [
-  {
-    id: "1",
-    name: "モーターヘッドモーターズ",
-    interviewDate: new Date("2022/8/11"),
-  },
-  { id: "2", name: "山下商事", interviewDate: new Date("2022/7/10") },
-  { id: "3", name: "大日本銀行", interviewDate: new Date("2022/8/1") },
-  { id: "4", name: "古海製薬", interviewDate: new Date("2022/9/29") },
-];
+const companies = testData;
 
-export const Original = () => <HomeContents companies={dummyData} />;
+export const Original = () => <HomeContents companies={companies} />;
