56fa94f89f42a0e2eb972544c40898973d87a597
cistT <cist.b220.t.chitose@gmail.com>
2022-08-17 19:26:51 +0900

    リストのタイトルを表示するコンポーネントを作成

---
+++ src/components/molecules/ListTitle/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { css } from "@emotion/react";
+
+import { Typography, Divider } from "@mui/material";
+
+export type ListTitleProps = {
+  title: string;
+  className?: string;
+};
+
+const ListTitle = (props: ListTitleProps) => {
+  const { title, className = "" } = props;
+  return (
+    <div css={styles.container}>
+      <Typography
+        variant="h1"
+        component="h1"
+        css={styles.title}
+        className={className}>
+        {title}
+      </Typography>
+      <Divider />
+    </div>
+  );
+};
+
+export default ListTitle;
+
+const styles = {
+  container: css`
+    text-align: center;
+  `,
+  title: css`
+    font-size: 32px;
+    margin-bottom: 10px;
+  `,
+};
