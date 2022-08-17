4dde4292d1216e045247392a77eb8c9ba63bf725
cistT <cist.b220.t.chitose@gmail.com>
2022-08-17 19:28:03 +0900

    企業情報をテストデータに変更

--- src/components/organisms/CompanyDateList/index.stories.tsx
+++ src/components/organisms/CompanyDateList/index.stories.tsx
@@ -3,23 +3,15 @@ import React from "react";
 import { ComponentMeta } from "@storybook/react";
 
 import CompanyDateList from ".";
+import testData from "data/testData";
 
 export default {
   title: "organisms/CompanyDateList",
   component: CompanyDateList,
 } as ComponentMeta<typeof CompanyDateList>;
 
-const dummyData = [
-  {
-    id: "1",
-    name: "モーターヘッドモーターズ",
-    interviewDate: new Date("2022/8/11"),
-  },
-  { id: "2", name: "山下商事", interviewDate: new Date("2022/7/10") },
-  { id: "3", name: "大日本銀行", interviewDate: new Date("2022/8/1") },
-  { id: "4", name: "古海製薬", interviewDate: new Date("2022/9/29") },
-];
+const companies = testData;
 
 export const Original = () => {
-  return <CompanyDateList companies={dummyData} />;
+  return <CompanyDateList companies={companies} />;
 };
