895a355e6c84c98e97dd6f9f0187190a00b41919
cistT <cist.b220.t.chitose@gmail.com>
2022-08-20 16:41:56 +0900

    登録した企業データのコンテキストを作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from "next/app";
 
 import "modern-css-reset/dist/reset.min.css";
 import Layout from "components/templetes/Layout";
+import testData from "data/testData";
+
+export const Companies = React.createContext(testData);
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <Companies.Provider value={testData}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </Companies.Provider>
 );
 
 export default MyApp;
