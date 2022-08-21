647960a20b4b1d33ee61b52a71a2fa72bd0cddd5
cistT <cist.b220.t.chitose@gmail.com>
2022-08-22 07:43:00 +0900

    Step4のStorybookを作成

---
+++ src/components/organisms/AddContents/Step4/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ComponentMeta } from "@storybook/react";
+
+import Step4 from ".";
+
+export default {
+  title: "organisms/AddContents/Step4",
+  component: Step4,
+} as ComponentMeta<typeof Step4>;
+
+export const Original = () => <Step4 />;
