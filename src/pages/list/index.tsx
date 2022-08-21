f48804eab20be648116911280f6b1277c2f30933
cistT <cist.b220.t.chitose@gmail.com>
2022-08-21 16:27:38 +0900

    企業一覧画面を作成

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -2,21 +2,30 @@ import React from "react";
 
 import Head from "next/head";
 
+import { Companies } from "pages/_app";
+
 import ListContents from "components/organisms/ListContents";
+import { useRouter } from "next/router";
 
-import { CompaniesType } from "types";
-
-type Props = { companies: CompaniesType[] };
-
-const Index = (props: Props) => {
-  const { companies } = props;
+const Index = () => {
+  const companies = React.useContext(Companies);
+  const router = useRouter();
 
   return (
     <>
       <Head>
-        <title>就活管理アプリ</title>
+        <title>企業一覧</title>
       </Head>
-      <ListContents companies={companies} />
+      <ListContents
+        companies={companies.map((data) => ({
+          ...data,
+          onClick: () =>
+            router.push({
+              pathname: "/list/[id]",
+              query: { id: data.id },
+            }),
+        }))}
+      />
     </>
   );
 };
