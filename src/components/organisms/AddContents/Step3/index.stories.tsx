21376533c7d1f85d015dd152506d62f968a97a91
cistT <cist.b220.t.chitose@gmail.com>
2022-08-22 07:42:27 +0900

    Step3のStorybookを作成

---
+++ src/components/organisms/AddContents/Step3/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ComponentMeta } from "@storybook/react";
+
+import Step3 from ".";
+
+export default {
+  title: "organisms/AddContents/Step3",
+  component: Step3,
+} as ComponentMeta<typeof Step3>;
+
+export const Original = () => <Step3 />;
