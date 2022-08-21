efa25000b7f7adf4218d57e68685e8b8ca88bc38
cistT <cist.b220.t.chitose@gmail.com>
2022-08-21 16:29:02 +0900

    ホーム画面を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,27 @@
 import React from "react";
 
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import Head from "next/head";
 
 import HomeContents from "components/organisms/HomeContents";
-import Link from "next/link";
+import { Companies } from "./_app";
 
 const Home: NextPage = () => {
-  const dummyData = [
-    {
-      id: "1",
-      name: "モーターヘッドモーターズ",
-      interviewDate: new Date("2022/8/11"),
-      result: "合格",
-    },
-    {
-      id: "2",
-      name: "山下商事",
-      interviewDate: new Date("2022/7/10"),
-      result: "合格",
-    },
-    {
-      id: "3",
-      name: "大日本銀行",
-      interviewDate: new Date("2022/8/1"),
-      result: "不合格",
-    },
-    {
-      id: "4",
-      name: "古海製薬",
-      interviewDate: new Date("2022/9/29"),
-      result: "未定",
-    },
-  ];
+  const router = useRouter();
+  const companies = React.useContext(Companies);
 
   return (
     <>
       <Head>
         <title>就活管理アプリ</title>
       </Head>
-      <HomeContents companies={dummyData} />
-      <Link href="/list">list</Link>
+      <HomeContents
+        companies={companies.map((data) => ({
+          ...data,
+          onClick: () => router.push("/list"),
+        }))}
+      />
     </>
   );
 };
