00ec83de75c58daa9834065cb11da9ac67462007
cistT <cist.b220.t.chitose@gmail.com>
2022-08-21 17:03:23 +0900

    メニューボタンを削除

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -8,15 +8,8 @@ import Typography from "@mui/material/Typography";
 
 import LoginButton from "components/atoms/Button/LoginButton";
 import LogoutButton from "components/atoms/Button/LogoutButton";
-import MenuButton from "components/atoms/Button/MenuButton";
 
-export type HeaderProps = {
-  visible?: boolean;
-};
-
-const Header = (props: HeaderProps) => {
-  const { visible = false } = props;
-
+const Header = () => {
   const [isLogin, setIsLogin] = React.useState(false);
   const login = () => setIsLogin(true);
   const logout = () => setIsLogin(false);
@@ -24,11 +17,7 @@ const Header = (props: HeaderProps) => {
   return (
     <header css={styles.header}>
       <AppBar>
-        <Toolbar css={styles.toolbar(visible)}>
-          {visible && (
-            <MenuButton onClick={() => undefined} css={styles.button} />
-          )}
-
+        <Toolbar css={styles.toolbar}>
           <Typography variant="h1" component="h1" css={styles.text}>
             就活管理アプリ
           </Typography>
@@ -50,10 +39,10 @@ const styles = {
   header: css`
     padding-bottom: 80px;
   `,
-  toolbar: (visible: boolean) => css`
+  toolbar: css`
     width: 100vw;
     display: grid;
-    grid-template-columns: ${visible ? "80px 1fr 100px" : "1fr 100px"};
+    grid-template-columns: 1fr 100px;
   `,
   button: css`
     color: white;
