69e0cfdbaed433931ec8754b15247ed901746786
cistT <cist.b220.t.chitose@gmail.com>
2022-08-22 07:43:29 +0900

    FinishのStorybookを作成

---
+++ src/components/organisms/AddContents/Finish/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ComponentMeta } from "@storybook/react";
+
+import Finish from ".";
+
+export default {
+  title: "organisms/AddContents/Finish",
+  component: Finish,
+} as ComponentMeta<typeof Finish>;
+
+export const Original = () => <Finish />;
