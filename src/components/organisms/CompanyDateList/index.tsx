3fc092cdc08f5db6844ae7c5a1660867c49b6756
cistT <cist.b220.t.chitose@gmail.com>
2022-08-28 13:16:14 +0900

    windowの横幅でListItemのitemの位置を変更できるようにした

--- src/components/organisms/CompanyDateList/index.tsx
+++ src/components/organisms/CompanyDateList/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { css } from "@emotion/react";
+
 import DateBox from "components/atoms/Box/DateBox";
 import CompanyListItem from "components/molecules/CompanyListItem";
 
 import { CompanyType } from "types";
+import useWindowSize from "hooks/useWindowSize";
 
 export type CompanyDateListProps = {
   companies: (CompanyType & {
@@ -15,6 +18,7 @@ export type CompanyDateListProps = {
 
 const CompanyList = (props: CompanyDateListProps) => {
   const { companies, className = "" } = props;
+  const { width } = useWindowSize();
 
   return (
     <div className={className}>
@@ -22,9 +26,12 @@ const CompanyList = (props: CompanyDateListProps) => {
         <CompanyListItem
           label={company.name}
           onClick={company.onClick}
-          rightItem={
-            company?.interviewDate && <DateBox date={company.interviewDate} />
+          item={
+            company?.interviewDate && (
+              <DateBox date={company.interviewDate} css={styles.date} />
+            )
           }
+          bottom={width < 600}
           className={company.className}
           key={company.id}
         />
@@ -34,3 +41,9 @@ const CompanyList = (props: CompanyDateListProps) => {
 };
 
 export default CompanyList;
+
+const styles = {
+  date: css`
+    font-size: clamp(1rem, 1vw + 1rem, 2.25rem);
+  `,
+};
