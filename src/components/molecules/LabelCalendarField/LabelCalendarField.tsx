b23d712b333d556a914d0007e0d349cec4215cb0
cistT <cist.b220.t.chitose@gmail.com>
2022-09-27 20:18:57 +0900

    質問とカレンダーから選択する回答を表示するコンポーネント

---
+++ src/components/molecules/LabelCalendarField/LabelCalendarField.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { css } from "@emotion/react";
+
+import { Typography } from "@mui/material";
+import { UseFormRegisterReturn } from "react-hook-form";
+import Calendar from "components/atoms/Calendar/Calendar";
+
+export type LabelCalendarFieldProps = {
+  label: string;
+  register: UseFormRegisterReturn;
+  className?: string;
+};
+
+const LabelCalendarField = (props: LabelCalendarFieldProps) => {
+  const { label, register, className = "" } = props;
+
+  return (
+    <div css={styles.container} className={className}>
+      <Typography variant="h3" component="h3" css={styles.explanation}>
+        {label}
+      </Typography>
+      <Calendar register={register} />
+    </div>
+  );
+};
+
+export default LabelCalendarField;
+
+const styles = {
+  container: css`
+    display: grid;
+    grid-template-rows: 30px 50px;
+    gap: 10px;
+  `,
+  explanation: css`
+    font-size: 20px;
+  `,
+  field: css`
+    width: 100%;
+  `,
+};
