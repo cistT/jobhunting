288f646228c79919b6ae68ce307dff7397821134
cistT <cist.b220.t.chitose@gmail.com>
2022-09-27 20:28:32 +0900

    技業情報を一時的に保存するHooksを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,10 @@ import Layout from "components/templetes/Layout";
 import useFetchRegisteredCompanyData, {
   CompanyDataWithUpDate,
 } from "hooks/useFetchRegisteredCompanyData";
+import useTemporarilySavedCompanyData, {
+  TemporarilySavedCompanyDataWithUpdate,
+} from "hooks/useTemporarilySavedCompanyData";
+import { CompanyType } from "types";
 
 export const Companies: React.Context<CompanyDataWithUpDate> =
   React.createContext<CompanyDataWithUpDate>({
@@ -14,15 +18,26 @@ export const Companies: React.Context<CompanyDataWithUpDate> =
     dispatchRegisteredCompanyData: () => undefined,
   });
 
+export const TemporarilyCompanies: React.Context<TemporarilySavedCompanyDataWithUpdate> =
+  React.createContext<TemporarilySavedCompanyDataWithUpdate>({
+    temporarilySavedCompanyData: {} as CompanyType,
+    dispatchTemporarilySavedCompanyData: () => undefined,
+  });
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const companyDataWithUpDate = useFetchRegisteredCompanyData();
+  const temporarilySavedCompanyDataWithUpdate =
+    useTemporarilySavedCompanyData();
 
   return (
-    <Companies.Provider value={companyDataWithUpDate}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Companies.Provider>
+    <TemporarilyCompanies.Provider
+      value={temporarilySavedCompanyDataWithUpdate}>
+      <Companies.Provider value={companyDataWithUpDate}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Companies.Provider>
+    </TemporarilyCompanies.Provider>
   );
 };
 
