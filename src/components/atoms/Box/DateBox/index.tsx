843109f9a042c6a2c0356f8cf434d7e5525b502b
cistT <cist.b220.t.chitose@gmail.com>
2022-10-09 13:07:53 +0900

    入力が文字列の場合も対応できるようにした

--- src/components/atoms/Box/DateBox/index.tsx
+++ src/components/atoms/Box/DateBox/index.tsx
@@ -4,7 +4,15 @@ import { css } from "@emotion/react";
 
 import { Typography } from "@mui/material";
 
-const extractDate = (date: Date) => {
+const extractDate = (date: Date | string) => {
+  if (typeof date === "string") {
+    const dates = new Date(date);
+    const year = dates.getFullYear();
+    const month = dates.getMonth() + 1;
+    const day = dates.getDate();
+
+    return { year, month, day };
+  }
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
