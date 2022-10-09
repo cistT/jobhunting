f7c12f97d55978e594ecb1ef32dcf6ce29293c48
cistT <cist.b220.t.chitose@gmail.com>
2022-10-09 13:14:25 +0900

    SSRでサーバからデータ取得できるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import Head from "next/head";
 
 import HomeContents from "components/organisms/HomeContents";
+import { CompanyType } from "types";
+
 import { Companies } from "./_app";
 
-const Home: NextPage = () => {
+type SSRProps = {
+  companyData: CompanyType[];
+};
+
+const Home: NextPage<SSRProps> = (props) => {
+  const { companyData } = props;
   const router = useRouter();
   const { registeredCompanyData } = React.useContext(Companies);
 
@@ -17,7 +24,7 @@ const Home: NextPage = () => {
         <title>就活管理アプリ</title>
       </Head>
       <HomeContents
-        companies={registeredCompanyData.map((data) => ({
+        companies={[...companyData, ...registeredCompanyData].map((data) => ({
           ...data,
           onClick: () => router.push(`/list/${data.id}`),
         }))}
@@ -27,3 +34,14 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps<SSRProps> = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_COMPANY_API_URL}/company/userID=1`,
+  );
+  const companyData = await res.json();
+
+  return {
+    props: { companyData: companyData.companyData },
+  };
+};
