7759d2c89a752146870781c6249b5b2b893b2d3c
cistT <cist.b220.t.chitose@gmail.com>
2022-10-25 13:33:52 +0900

    コンポーネント名を修正

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -5,7 +5,9 @@ import { useRouter } from "next/router";
 
 import { Companies } from "pages/_app";
 
-import ListContents from "components/organisms/ListContents";
+import ListTitle from "components/molecules/ListTitle";
+
+import ListItemBothEndsTextButton from "components/ListItem/ListItemBothEndsTextButton";
 
 const Index = () => {
   const { registeredCompanyData } = React.useContext(Companies);
@@ -16,16 +18,22 @@ const Index = () => {
       <Head>
         <title>企業一覧</title>
       </Head>
-      <ListContents
-        companies={registeredCompanyData.map((data) => ({
-          ...data,
-          onClick: () =>
+      <ListTitle title="企業一覧" />
+
+      {registeredCompanyData.map((data) => (
+        <ListItemBothEndsTextButton
+          label={data.name}
+          explanation={data.result}
+          onClick={() =>
             router.push({
               pathname: "/list/[id]",
               query: { id: data.id },
-            }),
-        }))}
-      />
+            })
+          }
+          underline
+          key={data.id}
+        />
+      ))}
     </>
   );
 };
