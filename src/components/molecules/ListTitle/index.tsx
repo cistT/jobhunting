47eefbd01eaf4c08f73a1ca8a6953076d85ae9e8
cistT <cist.b220.t.chitose@gmail.com>
2022-11-08 13:51:22 +0900

    propsで受け取ったstyleを適応する箇所を変更

--- src/components/molecules/ListTitle/index.tsx
+++ src/components/molecules/ListTitle/index.tsx
@@ -15,12 +15,12 @@ const ListTitle = (props: ListTitleProps) => {
   const { title, className = "" } = props;
 
   return (
-    <div className={styles["container"]}>
+    <div className={classNames(styles["container"], className)}>
       <Typography
         variant="h2"
         component="h2"
         css={styles.title}
-        className={classNames(styles["title"], className)}>
+        className={styles["title"]}>
         {title}
       </Typography>
       <Divider />
