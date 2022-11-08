f12e3a49f113787e1f93be73549648b9c4d13d36
cistT <cist.b220.t.chitose@gmail.com>
2022-11-08 14:02:12 +0900

    SSRを行い、企業情報の詳細を取得するように変更

--- src/pages/list/[id]/index.tsx
+++ src/pages/list/[id]/index.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 
-import { Companies } from "pages/_app";
-
 import useGetURLQuery from "hooks/useGetURLQuery";
 
+import { GetServerSideProps } from "next";
+import { CompanyType } from "types";
+import fetchCompanyDetail from "features/api/fetchCompanyDetail";
 import CompanyDitail from "features/CompanyDitail";
 
-const Index = () => {
-  const { id } = useGetURLQuery("id");
-  const { registeredCompanyData } = React.useContext(Companies);
-  const company = registeredCompanyData.filter((data) => data.id === id)[0];
+type SSRProps = {
+  company: CompanyType;
+};
 
-  return <CompanyDitail company={company} editHref={`/list/${id}/edit`} />;
+const Index = (props: SSRProps) => {
+  const { company } = props;
+
+  const { id } = useGetURLQuery("id");
+
+  if (company) {
+    return <CompanyDitail company={company} editHref={`/list/${id}/edit`} />;
+  }
+
+  return <div>Sorry</div>;
 };
 
 export default Index;
+
+export const getServerSideProps: GetServerSideProps<SSRProps> = async (
+  context,
+) => {
+  const id = (context?.params?.id as string) ?? "";
+  const company = await fetchCompanyDetail(id);
+
+  return {
+    props: { company },
+  };
+};
