e5ac24030f90d141e0bad49ec1167349ac00cc31
cistT <cist.b220.t.chitose@gmail.com>
2022-11-08 13:52:56 +0900

    削除ボタンを追加

--- src/components/templetes/DitailLayout/DitailLayout.tsx
+++ src/components/templetes/DitailLayout/DitailLayout.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import classNames from "classnames";
+
 import useGetURLQuery from "hooks/useGetURLQuery";
+import useBrowserBack from "hooks/useBrowserBack";
+
+import deleteCompany from "features/api/deleteCompany";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button } from "@mui/material";
 
 import ListTitle from "components/molecules/ListTitle";
 import DitailContentsDisplaySwitching from "components/organisms/DitailContentsDisplaySwitching/DitailContentsDisplaySwitching";
+
+import styles from "./DitailLayout.module.scss";
 
 export type DitaliLayoutProps = {
   companyName: string;
@@ -14,14 +24,29 @@ export type DitaliLayoutProps = {
 const DitailLayout = (props: DitaliLayoutProps) => {
   const { companyName, children, className } = props;
   const { id } = useGetURLQuery("id");
+  const { browserBack } = useBrowserBack();
   const buttons = [
     { label: "基本情報", href: `/list/${id}` },
     { label: "志望動機", href: `/list/${id}/motivation` },
   ];
 
+  const onClickDelete = async () => {
+    await deleteCompany("1", id as string);
+    browserBack();
+  };
+
   return (
-    <div className={className}>
-      <ListTitle title={companyName} />
+    <div className={classNames(styles["container"], className)}>
+      <div className={styles["upper"]}>
+        <ListTitle title={companyName} className={styles["title"]} />
+        <Button
+          onClick={onClickDelete}
+          className={styles["button"]}
+          variant="outlined">
+          <DeleteIcon fontSize="large" />
+        </Button>
+      </div>
+
       <DitailContentsDisplaySwitching buttons={buttons} />
       {children}
     </div>
